0c46c12356860a2478da8ada60bad8b863ba1a9a
Mehran Khan <i@mehrankhan.com>
2016-10-11 15:26:00 -0400

    Update nodejs-symbolise-global-packages.py

--- nodejs-symbolise-global-packages.py
+++ nodejs-symbolise-global-packages.py
@@ -6,8 +6,8 @@ def create_symbolic_link(globalModulePath):
 	path = os.path.join(globalModulePath, 'lib/node_modules/')
 	globalPackages = [anEntry for anEntry in os.listdir(globalModulePath) if os.path.isdir(os.path.join(globalModulePath,anEntry))]
 	for aPackage in globalPackages:
-		call(['ln', '-s', os.path.join(globalModulePath,aPackage,'bin',aPackage), os.path.join('/usr/bin/',aPackage)])
-		#print(" ".join(['ln', '-s', os.path.join(globalModulePath,aPackage,'bin',aPackage), os.path.join('/usr/bin/',aPackage)]))
+		if aPackage!='npm':
+			call(['ln', '-sf', os.path.join(globalModulePath,aPackage,'bin',aPackage), os.path.join('/usr/bin/',aPackage)])		
 
 if __name__ == '__main__':	
 	NODEJSROOT = '/opt/nodejs/lib/node_modules/'	
